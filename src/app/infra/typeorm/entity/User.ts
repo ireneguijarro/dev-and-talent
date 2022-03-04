d4db85678b4ab1137fead08cbfc39d921d61f640
Irene Guijarro Martínez <irene.guijarro@jobandtalent.com>
2022-03-04 13:33:27 +0100

    Update typeorm User schema to be functional

--- src/app/infra/typeorm/entity/User.ts
+++ src/app/infra/typeorm/entity/User.ts
@@ -1,19 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { EntitySchema } from 'typeorm';
+import { User } from '../../../core/entities/user';
 
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id!: string;
-
-  @Column()
-  name!: string;
-
-  @Column()
-  availability!: string;
-
-  @Column()
-  email!: string;
-
-  @Column()
-  country!: string;
-}
+export const UserEntity = new EntitySchema<User>({
+  name: 'user',
+  columns: {
+    id: {
+      type: 'uuid',
+      primary: true,
+      generated: 'uuid',
+    },
+    name: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    availability: {
+      type: String,
+    },
+  },
+});
--- src/app/infra/typeorm/repositories/UserRepository.ts
+++ src/app/infra/typeorm/repositories/UserRepository.ts
@@ -1,13 +1,13 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { User } from '../entity/User';
+import { AbstractRepository, EntityRepository } from 'typeorm';
+import { UserEntity } from '../entity/User';
 import { UserRepository as UserRepositoryAdapter } from '../../../core/repositories/user';
-import { User as UserModel } from '../../../core/entities/user';
+import { User } from '../../../core/entities/user';
 import { getConnection } from 'typeorm';
 
-@EntityRepository(User)
-export class UserRepository extends Repository<User> implements UserRepositoryAdapter {
-  async findAll(): Promise<UserModel[]> {
-    const repositoryORM = getConnection().getRepository(User);
+@EntityRepository(UserEntity)
+export class UserRepository extends AbstractRepository<User> implements UserRepositoryAdapter {
+  async findAll(): Promise<User[]> {
+    const repositoryORM = getConnection().getRepository<User>(UserEntity);
     const users = await repositoryORM.find();
     return users;
   }
